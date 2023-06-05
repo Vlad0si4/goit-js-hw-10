209394e08d98a3e607225e5766687bdb381fbf21
Vlad0si4 <boiko.vlad91@gmail.com>
2023-06-05 14:52:23 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -9,37 +9,42 @@ const error = document.querySelector('.error')
 const loader = document.querySelector('.loader')
 
 
+breedSelect.classList.add('is-hidden')
+catInfo.classList.add('is-hidden')
 
 fetchBreeds().then(breeds => {
 
-    error.classList.add('error')
-
   breeds.forEach(breed => {
+
     const option = document.createElement('option');
     option.value = breed.id;
     option.textContent = breed.name
     breedSelect.append(option)
-  });
-  new SlimSelect({
-    select: '#selectElement'
-  });
+});
+    new SlimSelect({
+      select: '#selectElement'
+    })
+
 }).catch(() => {
-   error.classList.add('error')
-   Notiflix.Notify.failure(`${error.innerText}`);
-}).finally(() => {
-  loader.classList.remove('loader');
-  })
-  
-  
+  error.classList.add('error')
+  Notiflix.Notify.failure(`${error.innerText}`);
+})
+  .finally(() => {
+      loader.classList.remove('loader');
+      })
+
+    
 const onSelectMarkup = event => {
 const selectBreed = event.target.value;
-loader.classList.add('loader');
-  setTimeout(() => {
+   
     fetchCatByBreed(selectBreed)
-    .then(data => {
+      .then(data => {
+      loader.classList.add('loader');
+      catInfo.classList.add('is-hidden')
+
 
       const catsData = data.map(cat =>
-        `<img src="${cat.url}" alt="Cat Image">
+        `<img src="${cat.url}" alt="${cat.breeds[0].name}">
         <h1>${cat.breeds[0].name}</h1>
         <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
         <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
@@ -50,10 +55,12 @@ loader.classList.add('loader');
    error.classList.add('error')
    Notiflix.Notify.failure(`${error.innerText}`);
     }).finally(() => {
+      setTimeout(() => {
       loader.classList.remove('loader');
-  })
-  },500)
-  
-  };
+      catInfo.classList.remove('is-hidden')
+      },500)
+       })
+
+    };
    breedSelect.addEventListener('change', onSelectMarkup);
 
